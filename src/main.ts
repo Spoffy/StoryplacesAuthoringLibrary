06b107f16ddb91fb6e21f64a8665171f49bba771
Spoffy <spoffeh@gmail.com>
2018-06-26 18:51:23 +0100

    Adds sample story

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,42 @@
-/* Code goes here */
+import {Story} from "./models/Story";
+import {Role} from "./models/Role";
+import {Page} from "./models/Page";
+import {PublishState} from "./schema/PublishState";
+import {PageHint} from "./models/PageHint";
+import {VariableReference} from "./models/VariableReference";
+import {VariableScope} from "./schema/VariableScopes";
+import {StoryFunctionSet} from "./models/StoryFunction";
+import {Audience} from "./schema/AudienceSchema";
+
+
+let story = new Story("Test Story");
+story.author = "Someone";
+story.schemaVersion = "";
+story.publishDate = "1/1/1111";
+story.publishState = PublishState.published;
+story.audience = Audience.general;
+story.roles.push(new Role("Main"));
+
+let var1 = new VariableReference(VariableScope.shared, "this", "magic");
+let setFunc = new StoryFunctionSet("setThing", var1, "false");
+let page1 = new Page({
+    name: "Test",
+    contentRef: "TestContent",
+    hint: new PageHint("Go north"),
+    functions: [setFunc]
+});
+story.pages.push(page1);
+story.content["TestContent"] = "bananas";
+
+let page2 = new Page({
+    name: "Page 2",
+    contentRef: "TestContent",
+    hint: new PageHint("Page2"),
+    functions: [setFunc],
+    singleVisit: true
+});
+story.pages.push(page2);
+
+let content = story.buildContent();
+
+console.log(JSON.stringify(content, null, 2));
