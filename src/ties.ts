dc92da1d14ea0b4741af41d88e999b272d2778b9
Spoffy <spoffeh@gmail.com>
2018-06-29 13:02:40 +0000

    Adds more ties story

--- src/ties.ts
+++ src/ties.ts
@@ -10,6 +10,10 @@ import {StoryCondition, StoryConditionComparison} from "./models/StoryCondition"
 import {ComparisonOperand, ComparisonType} from "./schema/ConditionSchema";
 import {Audience} from "./schema/AudienceSchema";
 
+/*** start - helper code to create phases and counter-locking ***/
+
+//helper code for phases
+
 interface Phase {
   var: VariableReference;
   enablePhaseFunction: StoryFunction;
@@ -33,59 +37,237 @@ function AddPagesToPhase(pages: Page[], phase: Phase): void {
   });
 }
 
+function CreateContent(story: Story, content: string): string {
+  story.content[content] = content;
+  return content;
+}
+
+interface Lock {
+	var: VariableReference;
+	lockFunction: StoryFunction;
+	lockedCondition: StoryCondition;
+}
+
+let lockCount: number = 0;
+
+function locks(lockingPages: Page[], pagesToLock: Page[]): Lock {
+  lockCount += 1;
+	let newVar = new VariableReference(VariableScope.shared, "locks", "lock" + lockCount);
+  let newLock = {
+		var: newVar,
+		lockFunction: new StoryFunctionSet("lockLock" + lockCount, newVar, "true"),
+		lockedCondition: new StoryConditionComparison(
+            					"isLock" + lockCount + "Locked", 
+            					ComparisonOperand.EQUAL,
+           						newVar,
+            					"true",
+            					ComparisonType.Variable,
+            					ComparisonType.String)
+	}
+	
+	lockingPages.forEach(page => {
+   		page.functions.push(newLock.lockFunction);
+	});
+  	
+	//set each of the lockedPages to check the lock
+	pagesToLock.forEach(page => {
+   		page.conditions.push(newLock.lockedCondition);
+	});
+	
+	return newLock;
+}
+
+let unlockCount: number = 0;
+
+function unlocks(unlockingPages: Page[], pagesToUnlock: Page[]): Lock {
+  unlockCount += 1;
+	let newVar = new VariableReference(VariableScope.shared, "unlocks", "lock" + lockCount);
+  let newLock = {
+		var: newVar,
+		lockFunction: new StoryFunctionSet("unlockLock" + lockCount, newVar, "true"),
+		lockedCondition: new StoryConditionComparison(
+            					"isLock" + lockCount + "Unlocked", 
+            					ComparisonOperand.EQUAL,
+           						newVar,
+            					"true",
+            					ComparisonType.Variable,
+            					ComparisonType.String)
+	}
+	
+	unlockingPages.forEach(page => {
+   		page.functions.push(newLock.lockFunction);
+	});
+  	
+	//set each of the lockedPages to check the lock
+	pagesToUnlock.forEach(page => {
+   		page.conditions.push(newLock.lockedCondition);
+	});
+	
+	return newLock;
+}
+
+/*** end - helper code to create phases ***/
+
+//story declaration
 let story = new Story("The Ties That Bind");
 story.author = "Callum Spawforth and David Millard";
 story.schemaVersion = "";
 story.publishDate = "1/7/2018";
 story.publishState = PublishState.published;
 story.audience = Audience.general;
-story.roles.push(new Role("Captor"));
-story.roles.push(new Role("Prisoner"));
+
+//set up roles and functions to check for roles
+let captorRole = new Role("Captor)")
+story.roles.push(captorRole);
+//let isCaptor = new StoryConditionIsRole("isCaptor", captorRole);
+
+let prisonerRole = new Role("Prisoner)")
+story.roles.push(prisonerRole);
+//let isPrisoner = new StoryConditionIsRole("isPrisoner", prisonerRole);
+
+// ********* INTRO - AND ROLE CHOICE *********
 
 
-//create the start and main phases for both roles (vars and funcs)
-let varCaptorMainPhase = new VariableReference(VariableScope.shared, "Captor", "varCaptorMainPhase")
-let setCaptorMainPhaseTrue = new StoryFunctionSet("setThing", varCaptorMainPhase, "true");
-let setCaptorMainPhaseFalse = new StoryFunctionSet("setThing", varCaptorMainPhase, "false");
-
-let varPrisonerMainPhase = new VariableReference(VariableScope.shared, "Prisoner", "varPrisonerMainPhase")
-let setPrisonerMainPhaseTrue = new StoryFunctionSet("setThing", varPrisonerMainPhase, "true");
-let setPrisonerMainPhaseFalse = new StoryFunctionSet("setThing", varPrisonerMainPhase, "false");
-
-let testPhase = CreatePhase("TestPhase");
-
-//create the initial page to choose roles
 let chooseRoles = new Page({
     name: "Please Choose Your Role",
-    contentRef: "Choose which role you would like to take in this story: Prisoner or Captor!",
+    contentRef: CreateContent(story, "Choose which role you would like to take in this story: Prisoner or Captor!"),
+	  singleVisit: true,
     hint: new PageHint("Select your role in the story..."),
-    functions: [testPhase.enablePhaseFunction]
 });
 story.pages.push(chooseRoles);
 
-
-
-//let varPrisonerMainPhase = new VariableReference(VariableScope.shared, "Captor", "varCaptorMainPhase")
-
-story.content["TestContent"] = "bananas";
-
-let page1 = new Page({
-    name: "Test Page 1",
-    contentRef: "TestContent",
-    hint: new PageHint("Go north")
+//intro page for the captor
+let chooseCaptor = new Page({
+    name: "The Captor",
+    contentRef: "Text introducing the captor...",
+    singleVisit: true,
+    hint: new PageHint("Choose to follow the captor")
 });
-story.pages.push(page1);
+//chooseCaptor.unlockedBy(chooseRoles);						***** WANT TO ADD THIS ****
+story.pages.push(chooseCaptor);
 
-let page2 = new Page({
-    name: "Test Page 2",
-    contentRef: "TestContent",
-    hint: new PageHint("Page2"),
+//intro page for the prisoner
+let choosePrisoner = new Page({
+    name: "The Prisoner",
+    contentRef: "Text introducing the prisoner...",
+    singleVisit: true,
+    hint: new PageHint("Choose to follow the prisoner")
+});
+//choosePrisoner.unlockedBy(chooseRoles);						***** WANT TO ADD THIS ****
+story.pages.push(choosePrisoner);
+
+
+
+
+// ********* ACT 1 - MORNING FOR THE CAPTOR / PRISONER *********
+
+//create the structures for the captor and prisoner at the same time as they mirror one another
+//we need two phases, as progress through the story is independent for each reader
+
+let morningPhaseCaptor = CreatePhase("MorningPhaseCaptor");
+chooseCaptor.functions.push(morningPhaseCaptor.enablePhaseFunction); //make sure its unlocked by the choose captor node
+
+let morningPhasePrisoner = CreatePhase("MorningPhasePrisoner");
+choosePrisoner.functions.push(morningPhasePrisoner.enablePhaseFunction); //make sure its unlocked by the choose prisoner node
+
+//initial options - available throughout the morning
+let whyHereCaptor = new Page({
+    name: "For the Cause",
+    contentRef: "Text explaining why the captor is doing this ...",
+    hint: new PageHint("there is always a reason"),
+    singleVisit: true,
+});
+
+let whyHerePrisoner = new Page({
+    name: "For the Cause",
+    contentRef: "Text explaining why the captor is doing this ...",
+    hint: new PageHint("they always have a reason"),
     singleVisit: true
 });
-story.pages.push(page2);
 
-AddPagesToPhase([page1, page2], testPhase);
 
+
+      					
+
+//one-off pages - alternatives that close off options for the other reader
+let offeringADrinkCaptor = new Page({
+    name: "I offered him a drink",
+    contentRef: "Text: captor helps the prisoner to take a drink",
+    hint: new PageHint("He'll pass out in this heat"),
+    singleVisit: true,
+});
+AddPagesToPhase([offeringADrinkCaptor], morningPhaseCaptor);
+
+let forcingADrinkCaptor = new Page({
+    name: "I forced him to drink",
+    contentRef: "Text: captor forced the prisoner to take a drink",
+    hint: new PageHint("I can't have him die on me"),
+    singleVisit: true,
+});
+AddPagesToPhase([forcingADrinkCaptor], morningPhaseCaptor);
+
+let offeringADrinkPrisoner = new Page({
+    name: "He offered me a drink",
+    contentRef: "Text: captor helps the prisoner to take a drink",
+    hint: new PageHint("I'll pass out in this heat"),
+    singleVisit: true,
+});
+AddPagesToPhase([offeringADrinkPrisoner], morningPhasePrisoner);
+
+let forcingADrinkPrisoner = new Page({
+    name: "He forced me to drink",
+    contentRef: "Text: captor forced the prisoner to take a drink",
+    hint: new PageHint("Is he worried that I'll die on him?"),
+    singleVisit: true,
+});
+AddPagesToPhase([forcingADrinkPrisoner], morningPhasePrisoner);
+
+locks([offeringADrinkCaptor, offeringADrinkPrisoner], 
+		[forcingADrinkCaptor, forcingADrinkPrisoner] );
+		
+locks([forcingADrinkCaptor, forcingADrinkPrisoner],
+		[offeringADrinkCaptor, offeringADrinkPrisoner] ); 
+
+
+
+//transition node - moves the story onwards to the afternoon (same for both roles)
+let moveToAfternoon = new Page({
+    name: "Noon",
+    contentRef: "Text explaining that time progresses to afternoon...",
+    hint: new PageHint("Is it noon already?")
+});
+
+
+
+//add the pages to the relevant morning phase
+AddPagesToPhase([whyHereCaptor, moveToAfternoon], morningPhaseCaptor);
+
+
+//******** this line causes an error ******
+//AddPagesToPhase([whyHerePrisoner, moveToAfternoon], morningPhasePrisoner);
+
+//add the pages to the story
+story.pages.push(whyHereCaptor, whyHerePrisoner, moveToAfternoon);
+
+
+
+
+// ********* ACT 2 - AFTERNOON FOR THE CAPTOR / PRISONER *********
+
+//create the structures for the captor and prisoner at the same time as they mirror one another
+
+let afternoonPhaseCaptor = CreatePhase("AfternoonPhaseCaptor");
+moveToAfternoon.functions.push(afternoonPhaseCaptor.enablePhaseFunction); //make sure its unlocked by the move to afternoon node
+
+let afternoonPhasePrisoner = CreatePhase("AfternoonPhasePrisoner");
+moveToAfternoon.functions.push(afternoonPhasePrisoner.enablePhaseFunction); //make sure its unlocked by the move to afternoon node
+
+
+
+
+// ********* FLASHBACK - IN THE VAN *********
+
+
+// output the JSON
 let content = story.buildContent();
-
 console.log(JSON.stringify(content, null, 2));
