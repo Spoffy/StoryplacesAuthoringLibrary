6e936f27c448b4b558415a1fd58412c375a1d3c0
Spoffy <spoffeh@gmail.com>
2019-04-11 19:35:39 +0100

    Adds role-specific nodes to main.js test story

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import {VariableReference} from "./models/VariableReference";
 import {VariableScope} from "./schemas/multiplayer/VariableScopes";
 import {StoryFunctionSet} from "./models/StoryFunction";
 import {Audience} from "./schemas/multiplayer/AudienceSchema";
-import { MultiplayerBuilder } from "./builders/Multiplayer";
+import {StoryPlacesBuilder} from "./builders/StoryPlaces";
+import {MultiplayerBuilder} from "./builders/Multiplayer";
 
 let story = new Story("Test Story");
 story.author = "Someone";
@@ -15,14 +16,35 @@ story.schemaVersion = "";
 story.publishDate = "1/1/1111";
 story.publishState = PublishState.published;
 story.audience = Audience.general;
-story.roles.push(new Role("Main"));
+
+let role1 = new Role("Role1");
+let role2 = new Role("Role2");
+story.roles.push(role1, role2);
+
+let setRole1Page = new Page({
+    name: "Set Role 1",
+    content: "Setting Role 1",
+    hint: new PageHint("Set to role 1"),
+    functions: [role1.getSetRoleFunction()]
+});
+story.pages.push(setRole1Page);
+
+let setRole2Page = new Page({
+    name: "Set Role 2",
+    content: "Setting Role 2",
+    hint: new PageHint("Set to role 2"),
+    functions: [role2.getSetRoleFunction()]
+});
+story.pages.push(setRole2Page);
 
 let var1 = new VariableReference(VariableScope.shared, "this", "magic");
 let setFunc = new StoryFunctionSet("setThing", var1, "false");
+
 let page1 = new Page({
-    name: "Test",
+    name: "Fayre St. Luke's Vicarage30",
     content: "TestContent",
     hint: new PageHint("Go north"),
+    conditions: [role1.getIsRoleCondition()],
     functions: [setFunc]
 });
 story.pages.push(page1);
@@ -32,6 +54,7 @@ let page2 = new Page({
     content: "TestContent",
     hint: new PageHint("Page2"),
     functions: [setFunc],
+    conditions: [role2.getIsRoleCondition()],
     singleVisit: true
 });
 story.pages.push(page2);
