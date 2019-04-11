e30aad0597e162fb89dc28d26e69550f82a16af3
Spoffy <spoffeh@gmail.com>
2019-04-11 19:35:11 +0100

    Adds getIsRoleCondition and getSetRoleFunction to Role class

--- lib/models/Role.d.ts
+++ lib/models/Role.d.ts
@@ -1,5 +1,9 @@
+import { StoryFunctionSetRole } from "./StoryFunction";
+import { StoryConditionIsRole } from "./StoryCondition";
 export declare class Role {
     id: string;
     required: boolean;
     constructor(id: string, required?: boolean);
+    getIsRoleCondition(): StoryConditionIsRole;
+    getSetRoleFunction(): StoryFunctionSetRole;
 }
--- src/models/Role.ts
+++ src/models/Role.ts
@@ -1,5 +1,16 @@
+import {StoryFunctionSetRole} from "./StoryFunction";
+import {StoryConditionIsRole} from "./StoryCondition";
+
 export class Role {
     constructor(
         public id: string,
         public required?: boolean) {}
+
+    getIsRoleCondition(): StoryConditionIsRole {
+        return new StoryConditionIsRole("Is" + this.id, this.id);
+    }
+
+    getSetRoleFunction(): StoryFunctionSetRole {
+        return new StoryFunctionSetRole("SetRoleTo" + this.id, this.id);
+    }
 }
